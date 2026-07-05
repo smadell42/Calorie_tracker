d8541c0adfca0a12d2664f8de2577f076aaf2ffd
Advaith123 <advaithsuresh20@gmail.com>
2026-07-05 23:35:57 +0530

    mobile app fix

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -6,19 +6,20 @@ import { Platform } from 'react-native';
 // If running on a physical device via Expo Go, it uses the packager's IP address automatically!
 // This means you don't have to hardcode your PC's IP address.
 const getApiBaseUrl = () => {
-  if (Platform.OS === 'web') {
-    return 'http://localhost:8000/api';
+  // If running locally in development (Expo Go), use the local packager IP.
+  // Otherwise, if this is a built production app, point to the live Render backend!
+  if (__DEV__) {
+    if (Platform.OS === 'web') return 'http://localhost:8000/api';
+    const hostUri = Constants.expoConfig?.hostUri;
+    if (hostUri) {
+      const ipAddress = hostUri.split(':')[0];
+      return `http://${ipAddress}:8000/api`;
+    }
+    return 'http://10.0.2.2:8000/api';
   }
   
-  // Get the IP address of the machine running the Expo server
-  const hostUri = Constants.expoConfig?.hostUri;
-  if (hostUri) {
-    const ipAddress = hostUri.split(':')[0];
-    return `http://${ipAddress}:8000/api`;
-  }
-  
-  // Fallback for Android emulator
-  return 'http://10.0.2.2:8000/api';
+  // PRODUCTION URL
+  return 'https://calorie-tracker-api-7ym2.onrender.com/api';
 };
 
 const API_BASE_URL = getApiBaseUrl();
